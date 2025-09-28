0cd342c7fa398ae099e4a56353ab9195d1735d86
Moses Thomas <78230723+xheenkhalil@users.noreply.github.com>
2025-09-28 11:51:47 +0100

    Update sitemap URLs to new domain

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,10 +2,10 @@ import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    { url: "https://yourdomain.com/", lastModified: new Date() },
-    { url: "https://yourdomain.com/projects", lastModified: new Date() },
-    { url: "https://yourdomain.com/resume", lastModified: new Date() },
-    { url: "https://yourdomain.com/skills", lastModified: new Date() },
+    { url: "https://mosesthomas.vercel.app/", lastModified: new Date() },
+    { url: "https://mosesthomas.vercel.app/projects", lastModified: new Date() },
+    { url: "https://mosesthomas.vercel.app/resume", lastModified: new Date() },
+    { url: "https://mosesthomas.vercel.app/skills", lastModified: new Date() },
   ];
 }
 
